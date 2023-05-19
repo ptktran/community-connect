adb8402bb7121afe6df2352fa6f5fb7e8a094776
Peter Tran <petertranminh22@gmail.com>
2023-05-19 12:48:55 -0400

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
