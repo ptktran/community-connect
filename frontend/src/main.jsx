6ee1eb5767ed14fa4637e188a2c98d99c7a7fb19
Kaung Si Thu <kaungsithu@berkeley.edu>
2023-05-19 17:38:37 -0700

    auth0 test

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="https://dev-0id2uicgi5udqk1l.us.auth0.com"
+    clientId="E0ZRQMB9VfXkeOFEr65n2bwghc7fNume"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
 )
